72dfb11577a4c158550c09c717ec4c34df51cc6f
DragoQC <alexisgauthier2001@hotmail.com>
2026-04-21 15:54:27 -0400

    Fixed invitation problems

--- managerwebapp/Services/InvitationEventsService.cs
+++ managerwebapp/Services/InvitationEventsService.cs
@@ -1,20 +1,30 @@
 namespace managerwebapp.Services;
 
-public sealed class InvitationEventsService
+public sealed class InvitationEventsService(ILogger<InvitationEventsService> logger)
 {
     public event Func<Task>? Changed;
 
-    public Task NotifyChangedAsync()
+    public async Task NotifyChangedAsync()
     {
         if (Changed is null)
         {
-            return Task.CompletedTask;
+            return;
         }
 
         IEnumerable<Func<Task>> handlers = Changed
             .GetInvocationList()
             .Cast<Func<Task>>();
 
-        return Task.WhenAll(handlers.Select(handler => handler()));
+        foreach (Func<Task> handler in handlers)
+        {
+            try
+            {
+                await handler();
+            }
+            catch (Exception exception)
+            {
+                logger.LogWarning(exception, "Invitation changed subscriber failed.");
+            }
+        }
     }
 }
